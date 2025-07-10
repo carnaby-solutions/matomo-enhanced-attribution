bf6c3c4c4476d35d9d01c462d5b0252f70656b68
Christian Bolstad <christian@carnaby.se>
2025-07-10 17:16:43 +0200

    Refactor main file

--- EnhancedAttribution.php
+++ EnhancedAttribution.php
@@ -4,7 +4,7 @@
  *
  * Enhanced attribution tracking and goal URL analysis
  * @link https://www.carnaby.se/matomo/plugins/enhancedattribution/
- * 
+ *
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
@@ -13,9 +13,17 @@ namespace Piwik\Plugins\EnhancedAttribution;
 use Piwik\Plugins\EnhancedAttribution\Commands\TestGoalUrlsPerformance;
 
 
+/**/
 
 class EnhancedAttribution extends \Piwik\Plugin
+
 {
+
+    /**
+     * Register plugin events
+     *
+     * @return string
+     */
     public function registerEvents()
     {
         return [
@@ -23,6 +31,11 @@ class EnhancedAttribution extends \Piwik\Plugin
         ];
     }
 
+    /**
+     * Add console commands to the application
+     *
+     * @param array $commands
+     */
     public function addConsoleCommands(&$commands)
     {
         $commands[] = new TestGoalUrlsPerformance();
